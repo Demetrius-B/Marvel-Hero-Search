ff24cbed1ac38424ae9b7761515ba6f666749cc4
Demetrius Berkeley <Dberkeley@fullsail.edu>
2017-08-12 19:36:45 -0400

    Switching over to windows

--- js/nav_animate.js
+++ js/nav_animate.js
@@ -1,8 +1,8 @@
 function toggle() {
-    var x = document.getElementById('menu');
+    const x = document.getElementById('menu');
     if (x.style.display === 'none') {
         x.style.display = 'flex';
-    } else {
-        x.style.display = 'none';
-    }
+     }  else {
+         x.style.display = 'none';
+     }
 }
